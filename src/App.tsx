4385a0bdc130bcca3ca10f68cb92e0352cadd14c
udaykumbhani999 <uday.k@simformsolutions.com>
2025-05-27 18:53:52 +0530

    added Accordion create

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
 import "./App.css";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center bg-gray-200">
-     
+      <div className="w-full max-w-xlg p-4">
+        <p className="bg-cyan-100 text-black text-2xl text-center">Accordion</p>
+        <Accordion
+          className="bg-gray-300 text-black p-10"
+          type="single"
+          collapsible
+        >
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-2">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-3">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
     </div>
   );
 }
 
 export default App;
+
